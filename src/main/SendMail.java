84037baff152c4284c46cac5e528e6d7cd89a376
budithikth <60140788+budithikth@users.noreply.github.com>
2020-01-30 22:34:30 +0100

    WIP Issue (#6)
    * Testing perms
    
    * feat adi testing email
    
    Co-authored-by: Adi Budithi <35716736+adibudithi@users.noreply.github.com>

--- src/main/SendMail.java
+++ src/main/SendMail.java
@@ -1,4 +1,17 @@
 package main;
 
 public class SendMail {
+  private String email;
+
+  public SendMail(String address) {
+    email = address;
+  }
+
+  public String setEmail(String address) {
+    email = address;
+  }
+
+  public String getEmail() {
+    return email;
+  }
 }
