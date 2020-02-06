a83631c70961b4b5d5abd308222430babb5b6360
budithikth <60140788+budithikth@users.noreply.github.com>
2020-02-06 12:10:52 +0100

    added jsonParser and writeToFile tests

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -2,6 +2,11 @@ package Test;
 
 import main.ContinuousIntegrationServer;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Test {
 
     //=============Test Cases Compile==================
@@ -58,12 +63,48 @@ public class Test {
 
     //=============Test Cases JSON==================
 
-    public void testJSONParsing() {
-
+    /*
+     * tests jsonParser function
+     */
+    public void testJSONParsing() throws IOException {
+        ContinuousIntegrationServer run = new ContinuousIntegrationServer();
+        Path path = Paths.get("exampleJSON.txt");
+        String content = Files.readString(path);
+        run.jsonParser(content);
+        if (run.clone_url.equals("https://github.com/Codertocat/Hello-World.git")) {
+            if (run.branch.equals("simple-tag")) {
+                if (run.email.equals("21031067+Codertocat@users.noreply.github.com")) {
+                    if (run.sha.equals("0000000000000000000000000000000000000000")) {
+                        System.out.println("Test Passed");
+                        return;
+                    }
+                }
+            }
+        }
+        System.out.println("Test Failed");
     }
 
-    public void testWriteToFile() {
-
+    /*
+     * tests writeToFile function
+     */
+    public void testWriteToFile() throws IOException {
+        ContinuousIntegrationServer run = new ContinuousIntegrationServer();
+        run.clone_url = "https://github.com/Codertocat/Hello-World.git";
+        run.branch = "simple-tag";
+        run.email = "21031067+Codertocat@users.noreply.github.com";
+        run.sha = "0000000000000000000000000000000000000000";
+        run.writeToFile("testHistory.txt");
+        run.sha = "1000000000000000000000000000000000000001";
+        run.writeToFile("testHistory.txt");
+        Path examplePath = Paths.get("exampleHistory.txt");
+        String example = Files.readString(examplePath);
+        Path testPath = Paths.get("testHistory.txt");
+        String test = Files.readString(testPath);
+        if (example.equals(test)) {
+            System.out.println(("Test Passed"));
+        } else {
+            System.out.println("Test Failed");
+        }
     }
 
     //=============Test Cases Compile==================
